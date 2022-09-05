9c87715121686e397c034e67f39947873b591fda
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-09-05 08:54:34 +0100

    created empty employee object that takes first, last name and annual salary + bonus

---
+++ PayCalc Project/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalc_Project
+{
+    class Employee
+    {
+        public string? firstName;
+        public string? lastName;
+        public double annualSalary;
+        public double annualBonus;
+
+        public Employee(string firstName, string lastName, double annualSalary, double annualBonus)
+        {
+
+        }
+    }
+}

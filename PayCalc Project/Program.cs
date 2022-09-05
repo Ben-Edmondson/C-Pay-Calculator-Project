866ffae4b86a85eb8f58625bb7334bf463adca80
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-09-05 12:29:13 +0100

    Main program can now output based on the input data

--- PayCalc Project/Program.cs
+++ PayCalc Project/Program.cs
@@ -10,8 +10,13 @@ namespace PayCalc_Project
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee();
-            Console.ReadKey();
+            Employee joeBloggs = new Employee() { typeOfEmployment = "Permanent", fname = "Joe", lname = "Bloggs", aSalary = 40000, aBonus = 5000 };
+            Employee johnSmith = new Employee() { typeOfEmployment = "Permanent", fname = "John", lname = "Smith", aSalary = 45000, aBonus = 2500 };
+            Employee clareJones = new Employee() { typeOfEmployment = "Temporary", fname = "Clare", lname = "Jones", dayRate = 350, weeksWorked = 40 };
+            Console.WriteLine($"User: {clareJones.fname} {clareJones.lname} Employment Type: {clareJones.typeOfEmployment} Annual Pay: {clareJones.TotalAnnualPay()} Hourly Rate: {clareJones.doubleHourlyRate()}");
+            Console.WriteLine($"User: {joeBloggs.fname} {joeBloggs.lname} Employment Type: {joeBloggs.typeOfEmployment} Annual Pay: {joeBloggs.TotalAnnualPay()} Hourly Rate: {joeBloggs.doubleHourlyRate()}");
+            Console.WriteLine($"User: {johnSmith.fname} {johnSmith.lname} Employment Type: {johnSmith.typeOfEmployment} Annual Pay: {johnSmith.TotalAnnualPay()} Hourly Rate: {johnSmith.doubleHourlyRate()}");
+            Console.ReadLine();
         }
     }
 }

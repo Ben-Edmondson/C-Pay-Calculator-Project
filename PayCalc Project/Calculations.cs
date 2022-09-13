0f346862ddf8c10e0d89f6aae62b7adfe0b5f987
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-09-13 13:44:26 +0100

    Small changes to calculations

--- PayCalc Project/Calculations.cs
+++ PayCalc Project/Calculations.cs
@@ -4,33 +4,31 @@
     { 
         public static decimal TotalAnnualPay(List<Employee>employees, int i)
         {
-            List<Employee> employeeList = employees;
-            if (employeeList[i].EmploymentType == TypeOfEmployment.Permanent)
+            if (employees[i].EmploymentType == TypeOfEmployment.Permanent)
             {
-                return employeeList[i].Salary + employeeList[i].Bonus;
+                return employees[i].Salary + employees[i].Bonus;
             }
             else
             {
                 int days = 5;
-                return Math.Round(employeeList[i].DayRate * (days * employeeList[i].WeeksWorked), 2);
+                return Math.Round(employees[i].DayRate * (days * employees[i].WeeksWorked), 2);
             }
 
         }
 
         public static decimal doubleHourlyRate(List<Employee> employees, int i)
         {
-            List<Employee> employeeList = employees;
-            if (employeeList[i].EmploymentType == TypeOfEmployment.Permanent)
+            if (employees[i].EmploymentType == TypeOfEmployment.Permanent)
             {
                 int weeks = 52;
                 int days = 5;
                 int hours = 7;
-                return Math.Round(employeeList[i].Salary / (days * hours) / weeks, 2);
+                return Math.Round(employees[i].Salary / (days * hours) / weeks, 2);
             }
             else
             {
                 int hours = 7;
-                return Math.Round(employeeList[i].DayRate / hours, 2);
+                return Math.Round(employees[i].DayRate / hours, 2);
             }
         }
     }

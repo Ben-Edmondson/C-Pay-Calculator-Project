ce9575a2882d00bfc4c77e3a6d611288446df67b
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-09-23 11:38:48 +0100

    Calculations Updated to return PostTaxValues

--- PayCalc Project/Services/Calculations.cs
+++ PayCalc Project/Services/Calculations.cs
@@ -9,7 +9,38 @@ namespace PayCalc_Project.Services
             decimal? Salary = employees[i].Salary;
             decimal? Bonus = employees[i].Bonus;
             decimal? Together = Salary + Bonus;
-            return Together;
+
+            int Basic = 20;
+            int Higher = 40;
+            int Additional = 45;
+            decimal? TaxedIncome = Together - 12570;
+            if (Together > 150000)
+            {
+                decimal? AdditionalRate = Together - 150000;
+                decimal? ResultAdditional = (AdditionalRate / 100) * 45;
+                decimal? HigherRate = 150000 - 50271;
+                decimal? ResultHigher = (HigherRate / 100) * 40;
+                decimal? BasicRate = 37701;
+                decimal? ResultBasic = (BasicRate / 100) * 20;
+                decimal? TotalTax = ResultAdditional + ResultHigher + ResultBasic;
+                return TotalTax;
+            }
+            else if (Together > 50271)
+            {
+                decimal? HigherRate = Together - 50271;
+                decimal? ResultHigher = (HigherRate / 100) * 40;
+                decimal? BasicRate = 50270 - 12570;
+                decimal? ResultBasic = (BasicRate / 100) * 20;
+                decimal? TotalTax = ResultBasic + ResultHigher;
+                return TotalTax;
+            }
+            else
+            {
+                decimal? BasicRate = Together - 12570;
+                decimal? ResultBasic = (BasicRate / 100) * 20;
+                decimal? TotalTax = ResultBasic;
+                return TotalTax;
+            }
         }
         public static decimal? TotalAnnualPayTemp(List<EmployeeTemp> employees, int i)
         {
@@ -18,7 +49,33 @@ namespace PayCalc_Project.Services
             int? WeeksWorked = employees[i].WeeksWorked;
            
             decimal? Total = Math.Round((decimal)(DayRate * (days * WeeksWorked)), 2);
-            return Total;
+            if (Total > 150000)
+            {
+                decimal? AdditionalRate = Total - 150000;
+                decimal? ResultAdditional = (AdditionalRate / 100) * 45;
+                decimal? HigherRate = 150000 - 50271;
+                decimal? ResultHigher = (HigherRate / 100) * 40;
+                decimal? BasicRate = 37701;
+                decimal? ResultBasic = (BasicRate / 100) * 20;
+                decimal? TotalTax = ResultAdditional + ResultHigher + ResultBasic;
+                return TotalTax;
+            }
+            else if (Total > 50271)
+            {
+                decimal? HigherRate = Total - 50271;
+                decimal? ResultHigher = (HigherRate / 100) * 40;
+                decimal? BasicRate = 50270 - 12570;
+                decimal? ResultBasic = (BasicRate / 100) * 20;
+                decimal? TotalTax = ResultBasic + ResultHigher;
+                return TotalTax;
+            }
+            else
+            {
+                decimal? BasicRate = Total - 12570;
+                decimal? ResultBasic = (BasicRate / 100) * 20;
+                decimal? TotalTax = ResultBasic;
+                return TotalTax;
+            }
         }
 
     }

41911adc4f7019be830c4487f7ff63cbf17ac22f
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-09-23 09:49:11 +0100

    Model updates.

--- PayCalc Project/Models/Employee.cs
+++ PayCalc Project/Models/Employee.cs
@@ -5,6 +5,5 @@
         public Guid ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName => $"{FirstName} {LastName}";
     }
 }
--- PayCalc Project/Models/EmployeeTemp.cs
+++ PayCalc Project/Models/EmployeeTemp.cs
@@ -4,5 +4,9 @@
     {
         public decimal? DayRate { get; set; }
         public int? WeeksWorked { get; set; }
+        public override string ToString()
+        {
+            return $"\nID: {ID} Name: {FirstName} {LastName} Day Rate: £{DayRate} Weeks Worked: {WeeksWorked}";
+        }
     }
 }

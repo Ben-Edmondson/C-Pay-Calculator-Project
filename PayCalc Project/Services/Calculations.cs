7847f43be02cf69df6d485ab6ba2bf5c64e8df09
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-10-04 10:16:05 +0100

    Tax calculator updated. Fixes required.

--- PayCalc Project/Services/Calculations.cs
+++ PayCalc Project/Services/Calculations.cs
@@ -6,42 +6,28 @@ namespace PayCalc_Project.Services
     {
         public static decimal? TotalAnnualPayPerm(List<EmployeePerm> employees, int i)
         {
-            decimal? Salary = employees[i].Salary;
-            decimal? Bonus = employees[i].Bonus;
-            decimal? Together = Salary + Bonus;
+            var taxBands = new[]
+            {
+            new { Lower = 0m, Upper = 12570m, Rate = 0.0m },
+            new { Lower = 12571m, Upper = 50270m, Rate = 0.2m },
+            new { Lower = 50271m, Upper = 150000m, Rate = 0.4m },
+            new { Lower = 150001m, Upper = decimal.MaxValue, Rate = 0.45m }
+            };
 
-            decimal noTax = 12570;
-            decimal BandOne = 12571;
-            decimal BandTwo = 50271;
-            decimal BandThree = 150001;
-            decimal? TaxedIncome = Together - 12570;
-            if (Together > 150000)
+            var salary = employees[i].Salary;
+
+            decimal? taxToBePaid = 0m;
+
+            foreach (var band in taxBands)
             {
-                decimal? AdditionalRate = Together - 150000;
-                decimal? ResultAdditional = (AdditionalRate / 100) * 45;
-                decimal? HigherRate = 150000 - 50271;
-                decimal? ResultHigher = (HigherRate / 100) * 40;
-                decimal? BasicRate = 50271 - 12570;
-                decimal? ResultBasic = (BasicRate / 100) * 20;
-                decimal? TotalTax = ResultAdditional + ResultHigher + ResultBasic;
-                return TotalTax;
+                if (salary > band.Lower)
+                {
+                    decimal? taxableAtThisRate = Math.Min(band.Upper - band.Lower, (byte)salary - band.Lower);
+                    decimal? taxThisBand = taxableAtThisRate * band.Rate;
+                    taxToBePaid += taxThisBand;
+                }
             }
-            else if (Together > 50270)
-            {
-                decimal? HigherRate = Together - 50271;
-                decimal? ResultHigher = (HigherRate / 100) * 40;
-                decimal? BasicRate = 50270 - 12570;
-                decimal? ResultBasic = (BasicRate / 100) * 20;
-                decimal? TotalTax = ResultBasic + ResultHigher;
-                return TotalTax;
-            }
-            else
-            {
-                decimal? BasicRate = Together - 12570;
-                decimal? ResultBasic = (BasicRate / 100) * 20;
-                decimal? TotalTax = ResultBasic;
-                return TotalTax;
-            }
+            return salary - taxToBePaid;
         }
         public static decimal? TotalAnnualPayTemp(List<EmployeeTemp> employees, int i)
         {
@@ -49,33 +35,28 @@ namespace PayCalc_Project.Services
             decimal? DayRate = employees[i].DayRate;
             int? WeeksWorked = employees[i].WeeksWorked;
             decimal? Total = Math.Round((decimal)(DayRate * (days * WeeksWorked)), 2);
-            if (Total > 150000)
+            var taxBands = new[]
             {
-                decimal? AdditionalRate = Total - 150000;
-                decimal? ResultAdditional = (AdditionalRate / 100) * 45;
-                decimal? HigherRate = 150000 - 50271;
-                decimal? ResultHigher = (HigherRate / 100) * 40;
-                decimal? BasicRate = 50270 - 12570;
-                decimal? ResultBasic = (BasicRate / 100) * 20;
-                decimal? TotalTax = ResultAdditional + ResultHigher + ResultBasic;
-                return TotalTax;
-            }
-            else if (Total > 50270)
+            new { Lower = 0m, Upper = 12570m, Rate = 0.0m },
+            new { Lower = 12571m, Upper = 50270m, Rate = 0.2m },
+            new { Lower = 50271m, Upper = 150000m, Rate = 0.4m },
+            new { Lower = 150001m, Upper = decimal.MaxValue, Rate = 0.45m }
+            };
+
+            var salary = Total;
+
+            decimal? taxToBePaid = 0m;
+
+            foreach (var band in taxBands)
             {
-                decimal? HigherRate = Total - 50271;
-                decimal? ResultHigher = (HigherRate / 100) * 40;
-                decimal? BasicRate = 50270 - 12570;
-                decimal? ResultBasic = (BasicRate / 100) * 20;
-                decimal? TotalTax = ResultBasic + ResultHigher;
-                return TotalTax;
+                if (salary > band.Lower)
+                {
+                    decimal? taxableAtThisRate = Math.Min(band.Upper - band.Lower, (byte)salary - band.Lower);
+                    decimal? taxThisBand = taxableAtThisRate * band.Rate;
+                    taxToBePaid += taxThisBand;
+                }
             }
-            else
-            {
-                decimal? BasicRate = Total - 12570;
-                decimal? ResultBasic = (BasicRate / 100) * 20;
-                decimal? TotalTax = ResultBasic;
-                return TotalTax;
-            }
+            return salary - taxToBePaid;
         }
 
     }

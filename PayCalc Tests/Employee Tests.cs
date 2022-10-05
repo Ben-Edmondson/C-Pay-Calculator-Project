4e3b12d8356b21d791012d51f8a6ea3536852f13
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-10-05 10:44:32 +0100

    Updated Employee Test Names

--- PayCalc Tests/Employee Tests.cs
+++ PayCalc Tests/Employee Tests.cs
@@ -9,32 +9,32 @@ namespace PayCalc_Tests
         EmployeePermRepo _repoPerm = new EmployeePermRepo();
         EmployeeTempRepo _repoTemp = new EmployeeTempRepo();
         [Test]
-        public void JoeBloggsDataTest()
+        public void JoeBloggsCalculationTestForTax()
         {
             //arrange
-            decimal? tAP = 6486;
+            decimal tAP = 6486;
             //act
-            decimal? tAPCorrect = Math.Round((decimal)Calculations.TotalAnnualPayPerm(_repoPerm.employees,0),0);
+            decimal tAPCorrect = Math.Round((decimal)Calculations.TotalAnnualPayPerm(_repoPerm.employees,0),0);
             //assert
             Assert.That(tAPCorrect, Is.EqualTo(tAP));
         }
         [Test]
-        public void JohnSmithDataTest()
+        public void JohnSmithDataCalculationTestForTax()
         {
             //arrange
-            decimal? tAP = 6986;
+            decimal tAP = 6986;
             //act
-            decimal? tAPCorrect = Math.Round((decimal)Calculations.TotalAnnualPayPerm(_repoPerm.employees,1),0);
+            decimal tAPCorrect = Math.Round((decimal)Calculations.TotalAnnualPayPerm(_repoPerm.employees,1),0);
             //assert
             Assert.That(tAPCorrect, Is.EqualTo(tAP));
         }
         [Test]
-        public void ClareJonesDataTest()
+        public void ClareJonesCalculationTestForTax()
         {
             //arrange
-            decimal? tAP = 15431M;
+            decimal tAP = 15431M;
             //act
-            decimal? tAPCorrect = Math.Round((decimal)Calculations.TotalAnnualPayTemp(_repoTemp.employees, 0),0);
+            decimal tAPCorrect = Math.Round((decimal)Calculations.TotalAnnualPayTemp(_repoTemp.employees, 0),0);
             //assert
             Assert.That(tAPCorrect, Is.EqualTo(tAP));
         }

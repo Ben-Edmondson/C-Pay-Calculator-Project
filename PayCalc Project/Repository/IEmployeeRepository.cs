adbfe7057b3929735eb92597bae522a44272fb3d
Ben Edmondson <Ben.Edmondson@tpr.gov.uk>
2022-10-05 10:46:16 +0100

    Comment Removal

--- PayCalc Project/Repository/IEmployeeRepository.cs
+++ PayCalc Project/Repository/IEmployeeRepository.cs
@@ -4,7 +4,7 @@
     {
         public T AddEmployee(string FirstName, string Surname, decimal? Salary, decimal? Bonus, decimal? DayRate, int? WeeksWorked);
         public List<T> ReadAll();
-        public T ReadSingle(int i); //string.ConCat
+        public T ReadSingle(int i);
         public T Update(int i, string FirstName, string LastName, decimal? Salary, decimal? Bonus, decimal? DayRate, int? WeeksWorked);
         public bool Delete(int i);
         public bool RemoveAll();

aecfd718f60caf5c3c9cff4523d4f063cbbac8ff
Ben Edmondson <ben.edmondson99@gmail.com>
2022-10-14 13:34:33 +0100

    Updated calculations to use ID instead of list index.

--- PayCalc Project/Services/PermCalculations.cs
+++ PayCalc Project/Services/PermCalculations.cs
@@ -1,32 +1,42 @@
 ﻿using PayCalc_Project.Models;
+using PayCalc_Project.Repository;
+
 namespace PayCalc_Project.Services
 {
     class PermCalculations : ICalculations<EmployeePerm>
     {
-        public decimal? TotalAnnualPay(List<EmployeePerm> employees, int index)
+        public decimal? TotalAnnualPay(List<EmployeePerm> employees, int id)
         {
-            var taxBands = new[]
+            foreach (EmployeePerm employee in employees)
             {
+                if (employee.ID == id)
+                {
+
+                    var taxBands = new[]
+                    {
             new { Lower = 0m, Upper = 12570m, Rate = 0.0m },
             new { Lower = 12571m, Upper = 50270m, Rate = 0.2m },
             new { Lower = 50271m, Upper = 150000m, Rate = 0.4m },
             new { Lower = 150001m, Upper = decimal.MaxValue, Rate = 0.45m }
             };
 
-            var salary = employees[index].Salary + employees[index].Bonus;
+                    var salary = employees[id].Salary + employees[id].Bonus;
 
-            decimal? taxToBePaid = 0m;
+                    decimal? taxToBePaid = 0m;
 
-            foreach (var band in taxBands)
-            {
-                if (salary > band.Lower)
-                {
-                    decimal? taxableAtThisRate = Math.Min(band.Upper - band.Lower, (decimal)salary - band.Lower);
-                    decimal? taxThisBand = taxableAtThisRate * band.Rate;
-                    taxToBePaid += taxThisBand;
+                    foreach (var band in taxBands)
+                    {
+                        if (salary > band.Lower)
+                        {
+                            decimal? taxableAtThisRate = Math.Min(band.Upper - band.Lower, (decimal)salary - band.Lower);
+                            decimal? taxThisBand = taxableAtThisRate * band.Rate;
+                            taxToBePaid += taxThisBand;
+                        }
+                    }
+                    return taxToBePaid;
                 }
             }
-            return taxToBePaid;
+            return null;
         }
     }
 }
--- PayCalc Project/Services/TempCalculations.cs
+++ PayCalc Project/Services/TempCalculations.cs
@@ -3,14 +3,18 @@ namespace PayCalc_Project.Services
 {
     public class TempCalculations : ICalculations<EmployeeTemp>
     {
-        public decimal? TotalAnnualPay(List<EmployeeTemp> employees, int index)
+        public decimal? TotalAnnualPay(List<EmployeeTemp> employees, int id)
         {
-            int? days = 5;
-            decimal? DayRate = employees[index].DayRate;
-            int? WeeksWorked = employees[index].WeeksWorked;
-            decimal? TotalPay = DayRate * (days * WeeksWorked);
-            var taxBands = new[]
+            foreach (EmployeeTemp employee in employees)
             {
+                if(employee.ID == id)
+                {
+                    int? days = 5;
+                    decimal? DayRate = employees[id].DayRate;
+                    int? WeeksWorked = employees[id].WeeksWorked;
+                    decimal? TotalPay = DayRate * (days * WeeksWorked);
+                    var taxBands = new[]
+                    {
             new { Lower = 0m, Upper = 12570m, Rate = 0.0m },
             new { Lower = 12571m, Upper = 50270m, Rate = 0.2m },
             new { Lower = 50271m, Upper = 150000m, Rate = 0.4m },
@@ -18,18 +22,21 @@ namespace PayCalc_Project.Services
             };
 
 
-            decimal? taxToBePaid = 0m;
+                    decimal? taxToBePaid = 0m;
 
-            foreach (var band in taxBands)
-            {
-                if (TotalPay > band.Lower)
-                {
-                    decimal? taxableAtThisRate = Math.Min(band.Upper - band.Lower, (decimal)TotalPay - band.Lower);
-                    decimal? taxThisBand = taxableAtThisRate * band.Rate;
-                    taxToBePaid += taxThisBand;
+                    foreach (var band in taxBands)
+                    {
+                        if (TotalPay > band.Lower)
+                        {
+                            decimal? taxableAtThisRate = Math.Min(band.Upper - band.Lower, (decimal)TotalPay - band.Lower);
+                            decimal? taxThisBand = taxableAtThisRate * band.Rate;
+                            taxToBePaid += taxThisBand;
+                        }
+                    }
+                    return taxToBePaid;
                 }
             }
-            return taxToBePaid;
+            return null;
         }
     }
 }

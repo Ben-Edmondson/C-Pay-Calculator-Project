b0d53c559f73c8794e2ee0256526f48c242c0979
Ben Edmondson <ben.edmondson99@gmail.com>
2022-10-17 10:41:37 +0100

    Minor fixes.

--- PayCalc Project/Repository/PermanentEmployeeRepo.cs
+++ PayCalc Project/Repository/PermanentEmployeeRepo.cs
@@ -26,8 +26,8 @@ namespace PayCalc_Project.Repository
         }
         public PermanentEmployee Create(string firstName, string lastName, decimal? salary, decimal? bonus, decimal? dayRate, int? weeksWorked)
         {
-            rnd.Next(1111, 10000);
-            PermanentEmployee employee = new PermanentEmployee() { ID = id, FirstName = firstName, LastName = lastName, Salary = salary, Bonus = bonus };
+
+            PermanentEmployee employee = new PermanentEmployee() { ID = rnd.Next(1111, 10000), FirstName = firstName, LastName = lastName, Salary = salary, Bonus = bonus };
             employees.Add(employee);
             return employee;
         }

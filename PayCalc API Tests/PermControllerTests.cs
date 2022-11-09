5d229e35f51fe479a846c1a5816f0cef35709c78
Ben Edmondson <ben.edmondson99@gmail.com>
2022-11-09 15:04:19 +0000

    Visibility.

--- PayCalc API Tests/PermControllerTests.cs
+++ PayCalc API Tests/PermControllerTests.cs
@@ -8,22 +8,24 @@ namespace PayCalc_API_Tests
 {
     public class PermControllerTests
     {
-        [SetUp]
-        public void Setup()
-        {
-            List<PermanentEmployee> employees = new List<PermanentEmployee>() {
+        List<PermanentEmployee> employees = new List<PermanentEmployee>() {
             new PermanentEmployee(){ ID = 1000, FirstName = "Joe", LastName = "Bloggs", Salary = 40000, Bonus = 5000 },
             new PermanentEmployee(){ ID = 1111, FirstName = "John", LastName = "Smith", Salary = 45000, Bonus = 2500 }};
 
-            Mock<IEmployeeRepository<PermanentEmployee>> mockPermanentRepository = new Mock<IEmployeeRepository<PermanentEmployee>>();
-            mockPermanentRepository.Setup(x => x.ReadAll()).Returns(employees);
-            mockPermanentRepository.Setup(y => y.Read(
+        Mock<IEmployeeRepository<PermanentEmployee>> _mockPermanentRepository = new Mock<IEmployeeRepository<PermanentEmployee>>();
+
+        [SetUp]
+        public void Setup()
+        {
+
+            _mockPermanentRepository.Setup(x => x.ReadAll()).Returns(employees);
+            _mockPermanentRepository.Setup(x => x.Read(
                 It.IsAny<int>())).Returns((int i) => employees.Where(
                 x => x.ID == i).Single());
-            mockPermanentRepository.Setup(z => z.Delete().Returns(true));
-            mockPermanentRepository.Setup(x => x.Create());
-            mockPermanentRepository.Setup(x => x.Update());
-            mockPermanentRepository.Setup(x => x.RemoveAll());
+            _mockPermanentRepository.Setup(x => x.Delete(1111)).Returns(true);
+            _mockPermanentRepository.Setup(x => x.Create());
+            _mockPermanentRepository.Setup(x => x.Update());
+            _mockPermanentRepository.Setup(x => x.RemoveAll());
             EmployeePermController permController = new EmployeePermController(mockPermanentRepository);
         }
 

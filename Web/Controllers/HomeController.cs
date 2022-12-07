dc98c07461e4f7ba8fae29c531df74f4c1c79617
Ben Edmondson <ben.edmondson99@gmail.com>
2022-12-07 10:54:02 +0000

    Created a test page.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

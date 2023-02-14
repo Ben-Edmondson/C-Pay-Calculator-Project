49f33a739f3df364cd4dd6ecbca8fc8bd2bd0cae
Ben Edmondson <ben.edmondson99@gmail.com>
2023-02-14 09:48:34 +0000

    Employee model now includes a Full Name attribute. Cards for employees now contain links to delete, update, and go back to employee list.

--- PayCalc Class Library/PayCalc Class Library/Models/Employee.cs
+++ PayCalc Class Library/PayCalc Class Library/Models/Employee.cs
@@ -5,5 +5,6 @@
         public int ID { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public string? Fullname => $"{FirstName} {LastName}";
     }
 }

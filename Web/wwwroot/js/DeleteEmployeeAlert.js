1dafce2b57b5051fba6a6107cf956146563c6030
Ben Edmondson <ben.edmondson99@gmail.com>
2023-02-22 10:17:50 +0000

    Added JS and alert to temp employee list. Non functional currently.

---
+++ Web/wwwroot/js/DeleteEmployeeAlert.js
@@ -0,0 +1,8 @@
+﻿function AlertSuccess() {
+    var x = document.getElementByID("alertDeleteSuccess");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}

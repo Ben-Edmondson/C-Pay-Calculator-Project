9cc62540856a4a7bd188a5b8216e4c1fbbe6c6bb
Ben Edmondson <ben.edmondson99@gmail.com>
2023-03-07 15:25:58 +0000

    Created Permanent and temporary web test files

---
+++ Web Tests/PermanentControllerWebTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using PayCalc_Project.Models;
+using PayCalc_Project.Repository;
+using Web.Controllers;
+
+
+namespace Web_Tests
+{
+    public class PermanentControllerWebTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void PermanentEmployeeList() 
+        {
+        
+        }
+    }
+}
---
+++ Web Tests/TemporaryControllerWebTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using PayCalc_Project.Models;
+using PayCalc_Project.Repository;
+using Web.Controllers;
+
+
+namespace Web_Tests
+{
+    public class TemporaryControllerWebTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TemporaryEmployeeList()
+        {
+
+        }
+    }
+}

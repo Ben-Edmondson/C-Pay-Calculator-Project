bd755593bcc51627700ac2bdf0ffa038eac27206
Ben Edmondson <ben.edmondson99@gmail.com>
2023-03-09 11:25:38 +0000

    Error messages now change based on Statuscode

--- Web/Controllers/ErrorController.cs
+++ Web/Controllers/ErrorController.cs
@@ -11,8 +11,18 @@ namespace Web.Controllers
 
         public IActionResult Error(int? statusCode = null)
         {
-                ViewBag.StatusCode = statusCode.Value;
-                return View();
+            ViewBag.StatusCode = statusCode.Value;
+            if (statusCode == 404)
+            {
+                ViewBag.ErrorMessage = "Not Found";
+            }else if (statusCode == 400)
+            {
+                ViewBag.ErrorMessage = "Bad Request";
+            }else if (statusCode == 500)
+            {
+                ViewBag.ErrorMessage = "Internal Server Error";
+            }
+            return View();
         }
     }
 }

67ac80018f08d3be8744696699181b864b496eb9
Ben Edmondson <ben.edmondson99@gmail.com>
2023-06-14 15:54:38 +0100

    Updated Tests to use TestCase

--- PayCalc Tests/DateCalculationTest.cs
+++ PayCalc Tests/DateCalculationTest.cs
@@ -1,17 +1,20 @@
 ﻿using PayCalc_Project.Models;
 using PayCalc_Project.Services;
+using NUnit.Framework;
 
 namespace PayCalc_Tests
 {
     [TestFixture]
     public class DateCalculationTest
     {
-        [Test]
-        public void TestDateCalculation() 
+        [TestCase("2023-01-01")]
+        [TestCase("2023-06-06")]
+        [TestCase("2022-01-01")]
+        public void TestDateCalculation(DateTime startDate) 
         {
             //arrange
             DateCalculator dateCalculations = new DateCalculator();
-            PermanentEmployee employee = new PermanentEmployee() { FirstName = "Test", LastName = "Test", ID = 0001, StartDate = new DateTime(2022, 01, 01) };
+            PermanentEmployee employee = new PermanentEmployee() { FirstName = "Test", LastName = "Test", ID = 0001, StartDate = startDate };
             //act
             var weeksWorkedSinceStart = dateCalculations.WeeksWorkedSinceStartDate(employee);
             TimeSpan timeSpan = DateTime.Today - employee.StartDate;

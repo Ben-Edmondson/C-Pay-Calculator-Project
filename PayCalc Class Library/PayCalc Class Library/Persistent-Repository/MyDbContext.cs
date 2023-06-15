e754f33f8379a076871a2f82c1243ae2b033bd2b
Ben Edmondson <ben.edmondson99@gmail.com>
2023-06-15 14:01:02 +0100

    dbcontext changes

--- PayCalc Class Library/PayCalc Class Library/Persistent-Repository/MyDbContext.cs
+++ PayCalc Class Library/PayCalc Class Library/Persistent-Repository/MyDbContext.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using PayCalc_Project.Models;
 
 namespace PayCalc_Class_Library.Persistent_Repository
 {
     public class MyDbContext : DbContext
     {
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        private readonly string connectionString;
+
+        public MyDbContext(string connectionString)
         {
+            this.connectionString = connectionString;
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("ConnectionString");
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
         public DbSet<PermanentEmployee>? PermanentEmployees { get; set; }

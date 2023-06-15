3d0258345a33dc2d2cd439512fa7672e71967a67
Ben Edmondson <ben.edmondson99@gmail.com>
2023-06-15 11:29:29 +0100

    Prep to create DB

---
+++ PayCalc Class Library/PayCalc Class Library/Persistent-Repository/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayCalc_Project.Models;
+
+namespace PayCalc_Class_Library.Persistent_Repository
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("ConnectionString");
+            }
+        }
+        public DbSet<PermanentEmployee>? PermanentEmployees { get; set; }
+        public DbSet<TemporaryEmployee>? TemporaryEmployees { get; set;}
+    }
+}
